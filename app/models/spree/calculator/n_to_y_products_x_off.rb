bb41518a3cd71eb5d2a128a7552191e2b0cd672a
Michael Bianco <iloveitaly@gmail.com>
2014-04-14 11:38:54 -0400

    Adding report for NtoYforXoff promotion

---
+++ app/models/spree/calculator/n_to_y_products_x_off.rb
@@ -0,0 +1,30 @@
+Spree::Calculator::NToYProductsXOff.class_eval do
+
+  def report(object = nil)
+    # TODO this needs to be much DRYer
+    more_count = 1
+
+    line_items_copy = object.line_items.map(&:dup)
+
+    line_items_copy.select { |l| self.matching_variants.include?(l.variant) }.sort_by { |l| l.variant.price }.each do |line_item|
+      discount = 0
+
+      break if more_count > self.preferred_y_items
+
+      line_item.quantity.times do
+        break if more_count > self.preferred_y_items
+
+        if more_count > self.preferred_n_items
+          discount += line_item.price * self.preferred_percent / 100.0
+        end
+
+        more_count += 1
+      end
+
+      line_item.price = (line_item.total - discount) / line_item.quantity
+    end
+
+    line_items_copy
+  end
+  
+end
